3894ba102929c7e7ded7fa581928c10cbc7359eb
BurriVik <burrivickram@gmail.com>
2021-11-16 18:55:24 +0530

    intial commit

---
+++ src/main/java/com/coder/user/autherization/service/user/authorization/service/UserAuthorizationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.coder.user.autherization.service.user.authorization.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserAuthorizationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserAuthorizationServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coder/user/autherization/service/user/authorization/service/UserAuthorizationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coder.user.autherization.service.user.authorization.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserAuthorizationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
